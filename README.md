c076d6407383934feb7f1ff71fd9f0c034460a78
NTHau <nthau18@apcs.fitus.edu.vn>
2022-04-11 20:58:26 +0700

    fix about responsive,portfolio section, next TODO: change portfolio items to list with js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 https://www.youtube.com/watch?v=G-Cr00UYokU
+
+<!--
+
+/* ========== Media Queries (MEDIUM DEVICES): ========== */
+@media screen and (max-width: 1024px) {
+
+}
+
+/* ========== Media Queries (SMALL DEVICES): ========== */
+@media screen and (max-width: 600px) {
+
+}
+
+ -->
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,172 @@
 import React from "react";
 
+import PORTFO1 from "../../assets/portfolio1.jpg";
+import PORTFO2 from "../../assets/portfolio2.jpg";
+import PORTFO3 from "../../assets/portfolio3.jpg";
+import PORTFO4 from "../../assets/portfolio4.jpg";
+import PORTFO5 from "../../assets/portfolio5.png";
+import PORTFO6 from "../../assets/portfolio6.jpg";
+
 import "./portfolio.css";
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO1} alt="" />
+          </div>
+          <h3>This is portfolio item title 1</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO2} alt="" />
+          </div>
+          <h3>This is portfolio item title 2</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO3} alt="" />
+          </div>
+          <h3>This is portfolio item title 3</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO4} alt="" />
+          </div>
+          <h3>This is portfolio item title 4</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO5} alt="" />
+          </div>
+          <h3>This is portfolio item title 5</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src={PORTFO6} alt="" />
+          </div>
+          <h3>This is portfolio item title 6</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
